740c3e4c4e57f0938b97855213ab3f99c39aed3a
popo26 <mynameispopo26@gmail.com>
2022-03-13 08:54:25 +1300

    commit after module completion

---
+++ lesson2/newscript.sh
@@ -0,0 +1,8 @@
+shell touch script_file echo bash is fun > script_file cp script_file script_backup ls -ltr
+curl -L git.io/unix echo repeat
+curl -L git.io/unix echo repeat
+curl -L git.io/unix echo repeat
+curl -L git.io/unix echo repeat
+curl -L git.io/unix echo repeat
+curl -L git.io/unix echo repeat
+shell touch script_file echo bash is fun > script_file cp script_file script_backup ls -ltr
